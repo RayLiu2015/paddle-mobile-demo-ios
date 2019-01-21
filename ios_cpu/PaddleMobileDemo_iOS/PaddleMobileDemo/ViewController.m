357735cada7f476924ec9522ee44b3317746e81c
liuruilong <liuruilong@baidu.com>
2019-01-21 20:48:15 +0800

    新添加测试代码

--- ios_cpu/PaddleMobileDemo_iOS/PaddleMobileDemo/ViewController.m
+++ ios_cpu/PaddleMobileDemo_iOS/PaddleMobileDemo/ViewController.m
@@ -12,38 +12,67 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License. */
 
-#import "PaddleMobile.h"
+#import "PaddleMobileCPU.h"
 #import "ViewController.h"
 
 @interface ViewController ()
+
+@property (strong, nonatomic) PaddleMobileCPU *paddleMobile;
+
+@property (strong, nonatomic) UIImage *testImage;
+
+@property (weak, nonatomic) IBOutlet UITextView *resultTextView;
+
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+
+@property (assign, nonatomic) BOOL loaded;
 
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
-    [super viewDidLoad];
-    PaddleMobile *pam = [[PaddleMobile alloc] init];
-    NSString *modelPath = [[NSBundle mainBundle] pathForResource:@"model" ofType:nil];
-    NSString *paramPath = [[NSBundle mainBundle] pathForResource:@"params" ofType:nil];
-    if (modelPath.length == 0 || paramPath.length == 0) {
-        NSLog(@" need model and param");
+    self.imageView.image = [UIImage imageNamed:@"apple.jpg"];
+}
+
+- (IBAction)loadAct:(id)sender {
+    PaddleMobileCPUConfig *config = [[PaddleMobileCPUConfig alloc] init];
+    config.threadNum = 1;
+    config.loddable = YES;
+    config.optimize = YES;
+
+    self.paddleMobile = [[PaddleMobileCPU alloc] initWithConfig:config];
+    
+    NSString *mobilenetModel = [[NSBundle mainBundle] pathForResource:@"ssd_model" ofType:nil];
+    
+    NSString *mobilenetParams = [[NSBundle mainBundle] pathForResource:@"ssd_params" ofType:nil];
+    
+    self.loaded = [self.paddleMobile loadModel:mobilenetModel andWeightsPath:mobilenetParams];
+}
+
+- (IBAction)clearAct:(id)sender {
+    [self.paddleMobile clear];
+}
+
+- (IBAction)predictAct:(id)sender {
+    if (!self.loaded) {
+        self.resultTextView.text = @" 还没有load, 需要 load ";
         return;
     }
     
-    if ([pam load:modelPath andWeightsPath:paramPath]) {
-        NSLog(@"load success");
-        UIImage *inputImage = [UIImage imageNamed:@"apple.jpg"];
-        if (!inputImage) {
-            NSLog(@" input image is nil");
-            return;
-        }
-        
-        NSDate *beforeDate = [NSDate date];
-        NSArray *res = [pam predict:inputImage.CGImage dim:@[@1, @3, @224, @224] means:@[@148, @148, @148] scale:1.0];
-        NSLog(@"res: %@", res);
-        NSLog(@"elapsed time: %f", [[NSDate date] timeIntervalSinceDate:beforeDate]);
+    if (!self.imageView.image) {
+        self.resultTextView.text = @" 图片为空 ";
+        return;
     }
+    
+    int max = 1;
+    NSDate *startDate = [NSDate date];
+    for (int i = 0; i < max; ++i) {
+        PaddleMobileCPUResult *res = [self.paddleMobile predict:self.imageView.image.CGImage dim:@[@(1), @(3), @(300), @(300)]];
+        [res releaseOutput];
+    }
+    NSTimeInterval t = [[NSDate date] timeIntervalSinceDate:startDate];
+    NSLog(@" time - %f", t/max);
 }
 
 @end
